3f02ee21155fb789e0f127dafabf18ecb0516350
RaquelAlbuquerque1 <albuquerqueraquel396@gmail.com>
2024-02-23 21:29:28 -0300

    feat: add theme, fonts and loading component

--- App.tsx
+++ App.tsx
@@ -1,5 +1,20 @@
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
+
 import { Groups } from "@screens/Groups";
+import { Loading } from "@components/Loading";
+import { ThemeProvider } from "styled-components";
+import theme from "./src/theme";
 
 export default function App() {
-  return <Groups />;
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  return (
+    <ThemeProvider theme={theme}>
+      {fontsLoaded ? <Groups /> : <Loading />}
+    </ThemeProvider>
+  );
 }
