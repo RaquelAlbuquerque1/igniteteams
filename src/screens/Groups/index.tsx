40d4348a5cfc78aaf719562328116dad962513b4
RaquelAlbuquerque1 <albuquerqueraquel396@gmail.com>
2024-02-26 20:49:49 -0300

    feat: create components

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,16 @@
 import { Container } from "./styles";
+import { GroupCard } from "@components/GroupCard";
 import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
 
 export function Groups() {
   return (
     <Container>
-      <Header />
+      <Header showBackButton />
+
+      <Highlight title="Turmas" subtitle="jogue com a sua turma" />
+
+      <GroupCard title="Galera do Ignite" />
     </Container>
   );
 }
